89c167c9c364f68fc5037beaecce91d0fc1e623e
Gabizinha1002 <gabriella.marques.silva@escola.pr.gov.br>
2024-09-21 15:22:08 -0300

    Add files via upload

--- js/feedback.js
+++ js/feedback.js
@@ -1,29 +1,28 @@
+
 const form = document.getElementById('opiniao-form');
+
 form.addEventListener('submit', async function (e) {
-    e.preventDefault();
+            e.preventDefault();
 
-    // Coletar os dados do formulário
-    const opiniao = document.getElementById('opiniao').value;
+            const opiniao = document.getElementById('opiniao').value;
+            try {
+                const response = await fetch('https://api.sheetmonkey.io/form/xfv8Kx1ihg6eHYj5tnAFop', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        opiniao: opiniao
+                    }),
+                });
 
-    // Enviar os dados para o Google Sheets via Sheet Monkey
-    try {
-        const response = await fetch('https://api.sheetmonkey.io/form/xfv8Kx1ihg6eHYj5tnAFop', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                opiniao: opiniao
-            }),
+                if (response.ok) {
+                    document.getElementById('mensagem-sucesso').style.display = 'block';
+                    form.reset();
+                } else {
+                    alert('Ocorreu um erro ao enviar sua opinião.');
+                }
+            } catch (error) {
+                alert('Erro de conexão, tente novamente.');
+            }
         });
-
-        if (response.ok) {
-            document.getElementById('mensagem-sucesso').style.display = 'block';
-            form.reset();
-        } else {
-            alert('Ocorreu um erro ao enviar sua opinião.');
-        }
-    } catch (error) {
-        alert('Erro de conexão, tente novamente.');
-    }
-});
