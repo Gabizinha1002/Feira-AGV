6405902bf630b893ca6a73faa5dfa81b092b6fba
Gabizinha1002 <gabriella.marques.silva@escola.pr.gov.br>
2024-09-22 11:23:32 -0300

    Add files via upload

--- js/materias.js
+++
@@ -1,7 +0,0 @@
-var elementosMaterias = document.querySelectorAll('.materias')
-
-elementosMaterias.forEach(function (materias){
-  materias.addEventListener('click', function (){
-   materias.classList.toggle('ativa')
-  })
-})
