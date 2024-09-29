45f2a0722019a1095e35e40ce516c91979fa6aa2
Gabizinha1002 <gabriella.marques.silva@escola.pr.gov.br>
2024-09-29 18:56:57 -0300

    Delete Feira-AGV-main directory

--- Feira-AGV-main/README.md
+++
@@ -1,1 +0,0 @@
-# Feira-AGV
--- Feira-AGV-main/js/materias.js
+++
@@ -1,7 +0,0 @@
-var elementosAnos = document.querySelectorAll('.anos')
-
-elementosAnos.forEach(function (anos){
-  anos.addEventListener('click', function (){
-   anos.classList.toggle('ativa')
-  })
-})
